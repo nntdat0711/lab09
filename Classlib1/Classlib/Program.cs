93ad901555b0030d3b10a913f7c4d08646e7442e
nntdat0711 <97040045+nntdat0711@users.noreply.github.com>
2022-11-10 17:31:18 +0700

    Add files via upload

---
+++ Classlib1/Classlib/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Classlib
+{
+    [TestClass]
+    public class TestStudent
+    {
+        [TestMethod]
+        public void Ifscore8_returnA()
+        {
+            Student s = new Student();
+            s.Score = 8;
+
+            char letter = s.getLetterScore();
+            Assert.AreEqual('A', letter);
+        }
+
+        [TestMethod]
+        public void Ifscore7_returnB()
+        {
+            Student s = new Student();
+            s.Score = 7;
+
+            char letter = s.getLetterScore();
+            Assert.AreEqual('B', letter);
+        }
+
+        [TestMethod]
+        public void Ifscore_returnA()
+        {
+            Student s = new Student();
+            s.Score = 5;
+
+            char letter = s.getLetterScore();
+            Assert.AreEqual('C', letter);
+        }
+    }
+}
---
+++ Classlib1/TestProject1/UnitTest1.cs
@@ -0,0 +1,42 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestClass]
+        public class TestStudent
+        {
+            [TestMethod]
+            public void Ifscore8_returnA()
+            {
+                Student s = new Student();
+                s.Score = 8;
+
+                char letter = s.getLetterScore();
+                Assert.AreEqual('A', letter);
+            }
+
+            [TestMethod]
+            public void Ifscore7_returnB()
+            {
+                Student s = new Student();
+                s.Score = 7;
+
+                char letter = s.getLetterScore();
+                Assert.AreEqual('B', letter);
+            }
+
+            [TestMethod]
+            public void Ifscore_returnA()
+            {
+                Student s = new Student();
+                s.Score = 5;
+
+                char letter = s.getLetterScore();
+                Assert.AreEqual('C', letter);
+            }
+        }
+    }
+}
